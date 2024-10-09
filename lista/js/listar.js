a4318d98768934a8c6b6d6c06752b2d68a333cf3
GeovaniPrestes <geovani@sistemasbr.net>
2024-10-09 09:24:51 -0300

    modificação(Listagem): Começando a mexer na página de listagem de participantes cadastrados.

--- lista/js/listar.js
+++ lista/js/listar.js
@@ -1,7 +1,27 @@
-var participantes = JSON.parse(localStorage.getItem('participantes')) || []
+var participantes = JSON.parse(localStorage.getItem("participantes")) || [];
 
-console.log(participantes);
-for(var i = 0; i < participantes.lenght; i++){
-    console.log(DeuCerto);
-    console.log(participantes);
+function CriarListaDeParticipantes() {
+  var tabela = document.getElementById("tabelaParticipantes");
+
+  Array.from(participantes).forEach(function (participante) {
+    if (!participante) {
+        var linha = document.createElement("tr");
+        var colunaImagem = document.createElement("td");
+        linha.appendChild(colunaImagem);
+        tabela.appendChild(linha);
+    } else {
+      var linha = document.createElement("tr");
+      var colunaImagem = document.createElement("td");
+      var imagemParticipante = document.createElement("img");
+      imagemParticipante.src = participante.imagem;
+      var colunaNome = document.createElement('td')
+      colunaNome.innerHTML = participante.nome;
+      colunaImagem.appendChild(imagemParticipante);
+      linha.appendChild(colunaImagem);
+      linha.appendChild(colunaNome)
+      tabela.appendChild(linha);
+    }
+  });
 }
+
+CriarListaDeParticipantes();
